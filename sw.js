31fb3f1706656459aa09c7af48e96e56b16cdc94
Simon Hofmann <mail@simon-hofmann.org>
2018-07-01 21:21:32 +0200

    Updates

--- sw.js
+++ sw.js
@@ -10,24 +10,24 @@ self.oninstall = (event) => {
             .then(cache => {
                     cache.addAll(
                         [
-                            '/',
-                            '/index.html',
-                            '/img/icons/app-icon-48x48.png',
-                            '/img/icons/app-icon-96x96.png',
-                            '/img/icons/app-icon-144x144.png',
-                            '/img/icons/app-icon-192x192.png',
-                            '/img/icons/app-icon-256x256.png',
-                            '/img/icons/app-icon-384x384.png',
-                            '/img/icons/app-icon-512x512.png',
-                            '/img/icons/apple-icon-76x76.png',
-                            '/img/icons/apple-icon-114x114.png',
-                            '/img/icons/apple-icon-144x144.png',
-                            '/img/icons/apple-icon-152x152.png',
-                            '/img/icons/apple-icon-60x60.png',
-                            '/img/icons/apple-icon-57x57.png',
-                            '/img/icons/apple-icon-72x72.png',
-                            '/img/icons/apple-icon-180x180.png',
-                            '/img/icons/apple-icon-120x120.png',
+                            './',
+                            './index.html',
+                            './img/icons/app-icon-48x48.png',
+                            './img/icons/app-icon-96x96.png',
+                            './img/icons/app-icon-144x144.png',
+                            './img/icons/app-icon-192x192.png',
+                            './img/icons/app-icon-256x256.png',
+                            './img/icons/app-icon-384x384.png',
+                            './img/icons/app-icon-512x512.png',
+                            './img/icons/apple-icon-76x76.png',
+                            './img/icons/apple-icon-114x114.png',
+                            './img/icons/apple-icon-144x144.png',
+                            './img/icons/apple-icon-152x152.png',
+                            './img/icons/apple-icon-60x60.png',
+                            './img/icons/apple-icon-57x57.png',
+                            './img/icons/apple-icon-72x72.png',
+                            './img/icons/apple-icon-180x180.png',
+                            './img/icons/apple-icon-120x120.png',
                         ]
                     )
                         .catch(error => console.log('Failed to initialize HTML cache:', error));
@@ -58,6 +58,7 @@ self.onactivate = (event) => {
 };
 
 self.onfetch = (event) => {
+    console.log(event);
     event.respondWith(
         fetch(event.request)
             .then(resp => {
