05179e3812fbd229846f0120613105c8e21fc96e
Simon Hofmann <mail@simon-hofmann.org>
2018-07-02 00:49:34 +0200

    Added prev and next buttons

--- src/components/imageshaker/imageshaker.js
+++ src/components/imageshaker/imageshaker.js
@@ -8,12 +8,15 @@ export default class ImageShaker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            payload: {}
+            history: [{}],
+            currentIndex: 0
         };
         this.fetchPayload(this.getDataUrl()).then(body => {
             this.setState({
+                history: [body],
+                currentIndex: this.state.history.length - 1,
                 maxComics: body.num
-            })
+            });
         });
     }
 
@@ -32,10 +35,17 @@ export default class ImageShaker extends React.Component {
 
     updateImage = () => {
         const url = this.getDataUrl(this.state.maxComics);
+        const currentHistory = this.state.history;
+        let lastIndex = this.state.history.length - 1;
 
         this.fetchPayload(url).then(body => {
             this.setState({
-                payload: body
+                history: currentHistory.concat(
+                    [
+                        body
+                    ]
+                ),
+                currentIndex: ++lastIndex
             });
         });
     };
@@ -50,16 +60,39 @@ export default class ImageShaker extends React.Component {
             );
     };
 
+    prev = () => {
+        let idx = this.state.currentIndex;
+        this.setState({
+            currentIndex: --idx
+        });
+    };
+
+    next = () => {
+        let idx = this.state.currentIndex;
+        this.setState({
+            currentIndex: ++idx
+        });
+    };
+
     render = () => {
+        const currentPayload = this.state.history[this.state.currentIndex];
+
+        console.log(this.state.currentIndex);
+        console.log(this.state.history);
         return (
             <div id={"comicContainer"}>
                 <div id={"imageContainer"}>
-                    <img className={"comic"} src={this.state.payload.img} alt={this.state.payload.alt}/>
+                    <img className={"comic"} src={currentPayload.img} alt={currentPayload.alt}/>
                     <div id={"comicName"}>
-                        <span>#{this.state.payload.num} | {this.state.payload.title}</span>
+                        <span>#{currentPayload.num} | {currentPayload.title}</span>
                     </div>
                 </div>
-                <button id={"shuffleBtn"} onClick={this.updateImage}>Get me another one!</button>
+                <div className={"buttongroup"}>
+                    { this.state.currentIndex > 1  && <button onClick={this.prev}>&lt;</button> }
+                    <button id={"shuffleBtn"} onClick={this.updateImage}>Get me another one!</button>
+                    { this.state.currentIndex < this.state.history.length - 1 && <button onClick={this.next}>&gt;</button> }
+                    <div className="clearer"></div>
+                </div>
             </div>
         )
     };
