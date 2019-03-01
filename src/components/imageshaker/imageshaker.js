e2801d57a0d02dac902b7827edb3ab467cbeffec
Simon Hofmann <simon.hofmann@consol.de>
2019-03-01 09:52:05 +0100

    Formatting

--- src/components/imageshaker/imageshaker.js
+++ src/components/imageshaker/imageshaker.js
@@ -1,99 +1,103 @@
 import React from "react";
 
-import './imageshaker.css';
-import currentConfig from '../../config';
+import "./imageshaker.css";
+import currentConfig from "../../config";
 
 export default class ImageShaker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{}],
+      currentIndex: 0
+    };
+    this.fetchPayload(this.getDataUrl()).then(body => {
+      this.setState({
+        history: [body],
+        currentIndex: this.state.history.length - 1,
+        maxComics: body.num
+      });
+    });
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: [{}],
-            currentIndex: 0
-        };
-        this.fetchPayload(this.getDataUrl()).then(body => {
-            this.setState({
-                history: [body],
-                currentIndex: this.state.history.length - 1,
-                maxComics: body.num
-            });
-        });
+  componentDidMount = () => {
+    this.updateImage();
+  };
+
+  getDataUrl = range => {
+    let url = currentConfig.dataUrl;
+    if (range) {
+      url += Math.floor(Math.random() * (range - 1) + 1);
     }
 
-    componentDidMount = () => {
-        this.updateImage();
-    };
+    return url;
+  };
 
-    getDataUrl = (range) => {
-        let url = currentConfig.dataUrl;
-        if (range) {
-            url += Math.floor((Math.random() * (range - 1)) + 1);
-        }
+  updateImage = () => {
+    const url = this.getDataUrl(this.state.maxComics);
+    const currentHistory = this.state.history;
+    let lastIndex = this.state.history.length - 1;
 
-        return url;
-    };
+    this.fetchPayload(url).then(body => {
+      this.setState({
+        history: currentHistory.concat([body]),
+        currentIndex: ++lastIndex
+      });
+    });
+  };
 
-    updateImage = () => {
-        const url = this.getDataUrl(this.state.maxComics);
-        const currentHistory = this.state.history;
-        let lastIndex = this.state.history.length - 1;
+  fetchPayload = url => {
+    return fetch(url)
+      .then(response => response.json())
+      .then(body => body)
+      .catch(err => console.log(err));
+  };
 
-        this.fetchPayload(url).then(body => {
-            this.setState({
-                history: currentHistory.concat(
-                    [
-                        body
-                    ]
-                ),
-                currentIndex: ++lastIndex
-            });
-        });
-    };
+  prev = () => {
+    let idx = this.state.currentIndex;
+    this.setState({
+      currentIndex: --idx
+    });
+  };
 
-    fetchPayload = (url) => {
-        return fetch(url)
-            .then(response => response.json()
-                .then(body => {
-                    return body;
-                })
-                .catch(err => console.log(err))
-            );
-    };
+  next = () => {
+    let idx = this.state.currentIndex;
+    this.setState({
+      currentIndex: ++idx
+    });
+  };
 
-    prev = () => {
-        let idx = this.state.currentIndex;
-        this.setState({
-            currentIndex: --idx
-        });
-    };
+  render = () => {
+    const currentPayload = this.state.history[this.state.currentIndex];
 
-    next = () => {
-        let idx = this.state.currentIndex;
-        this.setState({
-            currentIndex: ++idx
-        });
-    };
-
-    render = () => {
-        const currentPayload = this.state.history[this.state.currentIndex];
-
-        console.log(this.state.currentIndex);
-        console.log(this.state.history);
-        return (
-            <div id={"comicContainer"}>
-                <div id={"imageContainer"}>
-                    <img className={"comic"} src={currentPayload.img} alt={currentPayload.alt}/>
-                    <div id={"comicName"}>
-                        <span>#{currentPayload.num} | {currentPayload.title}</span>
-                    </div>
-                </div>
-                <div className={"buttongroup"}>
-                    { this.state.currentIndex > 1  && <button onClick={this.prev}>&lt;</button> }
-                    <button id={"shuffleBtn"} onClick={this.updateImage}>Get me another one!</button>
-                    { this.state.currentIndex < this.state.history.length - 1 && <button onClick={this.next}>&gt;</button> }
-                    <div className="clearer"></div>
-                </div>
-            </div>
-        )
-    };
+    console.log(this.state.currentIndex);
+    console.log(this.state.history);
+    return (
+      <div id={"comicContainer"}>
+        <div id={"imageContainer"}>
+          <img
+            className={"comic"}
+            src={currentPayload.img}
+            alt={currentPayload.alt}
+          />
+          <div id={"comicName"}>
+            <span>
+              #{currentPayload.num} | {currentPayload.title}
+            </span>
+          </div>
+        </div>
+        <div className={"buttongroup"}>
+          {this.state.currentIndex > 1 && (
+            <button onClick={this.prev}>&lt;</button>
+          )}
+          <button id={"shuffleBtn"} onClick={this.updateImage}>
+            Get me another one!
+          </button>
+          {this.state.currentIndex < this.state.history.length - 1 && (
+            <button onClick={this.next}>&gt;</button>
+          )}
+          <div className="clearer" />
+        </div>
+      </div>
+    );
+  };
 }
